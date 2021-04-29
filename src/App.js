286f33c3659a6b9f3b00d4b468d6d944d39d1b8d
Galen Montrone <gjmontrone@gmail.com>
2021-04-29 19:18:02 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import JohnWesleyHardin from "./components/JohnWesleyHardin";
 import WildBill from "./components/WildBill";
 import JessieJames from "./components/JessieJames";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,41 +23,41 @@ class App extends Component {
 
 render () {
   return (
-    <div>
-      <Navbar>
-        <NavbarBrand href="/"></NavbarBrand>
-      </Navbar>
-      <Container>
-        <Row>
-          <img src="assets/images/WildWestLogo.png" max width="100%"></img>
-        </Row>
-        <Row>
-          <Col>
-            <BillyTheKid figures={this.state.wildwestfigures} />
-          </Col>
-        </Row>
-        <Row>
-          <Col xl="8">
-            <Row>
-              <Col>
-                <WyattEarp figures={this.state.wildwestfigures} />
-              </Col>
-            </Row>
-            <Row>
-              <Col xl="6">
-                <JessieJames figures={this.state.wildwestfigures} />
-              </Col>
-              <Col xl="6">
-                <WildBill figures={this.state.wildwestfigures} />
-              </Col>
-            </Row>
-          </Col>
+    <div className="Black">
+        <Navbar className="Bar">
+          <NavbarBrand className="logo" href="/">American Crime</NavbarBrand>
+        </Navbar>
+        <Container className="body">
+          <Row>
+            <img src="assets/images/WildWestLogo.png" max width="100%"></img>
+          </Row>
+          <Row>
+            <Col>
+              <BillyTheKid figures={this.state.wildwestfigures} />
+            </Col>
+          </Row>
+          <Row>
+            <Col xl="8">
+              <Row>
+                <Col>
+                  <WyattEarp figures={this.state.wildwestfigures} />
+                </Col>
+              </Row>
+              <Row>
+                <Col xl="6">
+                  <JessieJames figures={this.state.wildwestfigures} />
+                </Col>
+                <Col xl="6">
+                  <WildBill figures={this.state.wildwestfigures} />
+                </Col>
+              </Row>
+            </Col>
 
-          <Col xl="4">
-            <JohnWesleyHardin figures={this.state.wildwestfigures} />
-          </Col>
-        </Row>
-      </Container>
+            <Col xl="4">
+              <JohnWesleyHardin figures={this.state.wildwestfigures} />
+            </Col>
+          </Row>
+        </Container>
     </div>
   );
 }
